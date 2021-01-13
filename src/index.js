97cbcb89dea7743504cf9450e689f0ecbc6a41da
jim <wowdacom@gmail.com>
2021-01-13 17:40:19 +0800

    update loader

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import _ from "lodash";
 import "../style/file.css";
 
-function component() {
-  const element = document.createElement("div");
+var MyApp = {};
+MyApp.MyModule = (function () {
+  const loadImage = () => {
+    let base64link1 = require("../images/app-store.png");
+    let myImg1 = document.querySelector(".app-store-image");
+    myImg1.setAttribute("src", base64link1.default);
 
-  // instead of Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack", "I am coming!"], " ");
-  element.classList.add("mystyle");
+    let base64link2 = require("../images/google-play.png");
+    let myImg2 = document.querySelector(".google-play-image");
+    myImg2.setAttribute("src", base64link2.default);
+  };
 
-  return element;
-}
+  return {
+    loadImage,
+  };
+})();
 
-document.body.appendChild(component());
+MyApp.MyModule.loadImage();
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,15 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "static/js/[name].[hash].js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 8080,
   },
   module: {
     rules: [
@@ -32,6 +37,26 @@ module.exports = {
                   ],
                 ],
               },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|jpe?g|svg)$/,
+        use: [
+          {
+            // 直接配置 url-loader 就好，超過上限的資源會自動 fallback 給 file-loader
+            loader: "url-loader",
+            options: {
+              name: "images/[name].[ext]",
+              limit: 10000,
+              publicPath: "images/",
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
             },
           },
         ],
